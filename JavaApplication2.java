7d44e4d5f8f7da353331e8379473e186d21f6087
ilham mahadika <ilham.mahadhika@gmail.com>
2016-03-29 09:39:44 +0700

    Added files via upload

---
+++ JavaApplication2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikum
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=0, b=1, hasil =0, c;
+        Scanner input = new Scanner(System.in);
+         System.out.print("input : ");
+         c = input.nextInt();
+         for(int i = 1; i <= c; i++){
+             a = b;
+             b = hasil;
+             System.out.print(hasil + " ");
+             hasil = a+b;
+         }
+    }
+    
+}
